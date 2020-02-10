6cbf2094b3d531767f0bcd78a14a479af368112c
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-10 14:00:20 -0500

    Added Hashes

--- hashes.py
+++ hashes.py
@@ -1,2 +1,14 @@
 import hashlib
 
+key = b"str"
+my_string="THis is a normal string. Nothingto see here".encode()#Turns to a bytes like object
+
+# for i in range(10):
+#     hashed=hashlib.sha256(key).hexdigest()
+#     print(hashed)
+    # breakpoint()
+
+for i in range(10):
+    hashed=hash(key)
+    print(hashed%8)
+    
